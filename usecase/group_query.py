3264540168bc626f5f9e267534d173beb0320037
Azamat <azamat201760@yandex.ru>
2025-10-11 23:44:42 +0300

    [add] group query usecase

--- usecase/group_query.py
+++ usecase/group_query.py
@@ -1,5 +1,6 @@
 from uuid import UUID
 
+from entity.form import Form
 from entity.group import Group
 from usecase.interface import GroupRepository
 
@@ -8,18 +9,12 @@ class GroupQuery:
     def __init__(self, repo: GroupRepository):
         self.repo = repo
 
-    def create_group(self, group: Group) -> None:
-        self.repo.create_group(group)
+    def get(self, group_id: UUID) -> Group:
+        return self.repo.get(group_id)
 
-    def get_group(self, group_id: UUID) -> Group:
-        return self.repo.get_group(group_id)
+    def delete(self, owner_id: UUID) -> None:
+        # TODO: Добавить каскадное удаление из группы всех участников
+        self.repo.delete_by_owner_id(owner_id)
 
-    def update_group(self, group: Group) -> None:
-        group.update_timestamp()
-        self.repo.update_group(group)
-
-    def delete_group(self, group_id: UUID) -> None:
-        self.repo.delete_group(group_id)
-
-    def list_groups(self) -> list[Group]:
-        return self.repo.list_groups()
+    def list_members(self, group_id: UUID) -> list[Form]:
+        return self.repo.list_members(group_id)
