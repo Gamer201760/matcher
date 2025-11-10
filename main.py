c5f485c2d5040dec155bfaf1b68551fb35decf07
Azamat <azamat201760@yandex.ru>
2025-11-10 11:04:07 +0300

    [add] reflection

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import os
 from concurrent import futures
 
 import grpc
+from grpc_reflection.v1alpha import reflection
 
+import gen.matcher.matcher_pb2 as pb2
 import gen.matcher.matcher_pb2_grpc as pb2_grpc
 from adapter.grpc.server import (
     FindGroupServicer,
@@ -59,6 +61,12 @@ def main():
         GroupQueryServicer(group_q), server
     )
     pb2_grpc.add_GroupServiceServicer_to_server(GroupServicer(group_serv), server)
+
+    service_names = tuple(
+        s.full_name for s in pb2.DESCRIPTOR.services_by_name.values()
+    ) + (reflection.SERVICE_NAME,)
+    reflection.enable_server_reflection(service_names, server)
+
     # Привязка к порту
     server.add_insecure_port('0.0.0.0:50051')
 
